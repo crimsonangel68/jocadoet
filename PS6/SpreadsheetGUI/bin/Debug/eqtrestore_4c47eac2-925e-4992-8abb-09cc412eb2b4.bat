61e6b642e9a348e74931bbe8ca149bcf3903ca32
FieldBase <josh.a.boren@gmail.com>
2013-03-31 15:36:14 -0600

    Folders

---
+++ PS6/SpreadsheetGUI/bin/Debug/eqtrestore_4c47eac2-925e-4992-8abb-09cc412eb2b4.bat
@@ -0,0 +1,4 @@
+REM This file restores the in-place instrumented code coverage binaries
+
+
+COPY EQT_4c47eac2-925e-4992-8abb-09cc412eb2b4\SpreadsheetGUI.exe . 
---
+++ PS6/SpreadsheetGUI/bin/Debug/eqtrestore_58218086-d8e4-414a-ae29-c4adce85336f.bat
@@ -0,0 +1,4 @@
+REM This file restores the in-place instrumented code coverage binaries
+
+
+COPY EQT_58218086-d8e4-414a-ae29-c4adce85336f\SpreadsheetGUI.exe . 
