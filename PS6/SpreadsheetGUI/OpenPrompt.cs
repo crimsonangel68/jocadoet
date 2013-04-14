f726cdaf0140c9257e95db02e96c3af5f28d7efa
Calvin Kern <calvinmkern@gmail.com>
2013-04-13 18:13:06 -0600

    almost finished pre-spreadsheet clientside communication. Waiting on server...

--- PS6/SpreadsheetGUI/OpenPrompt.cs
+++ PS6/SpreadsheetGUI/OpenPrompt.cs
@@ -15,6 +15,7 @@ namespace SpreadsheetGUI
     public partial class OpenPrompt : Form
     {
         SSModel model;
+        
         /// <summary>
         /// This method will open a new prompt window, which a user
         ///  will be able to enter in a file to either create a new
@@ -27,24 +28,105 @@ namespace SpreadsheetGUI
             model = newModel;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void NewButton_Click(object sender, EventArgs e)
         {
             String message = "CREATE\n";
             message += "Name:" + FileNameTextBox.Text + "\n";
             message += "Password:" + PasswordTextBox.Text + "\n";
             model.topModel.socket.BeginSend(message, (f, p) => { }, 0);
-            model.topModel.socket.BeginReceive(lineReceived, 0);
+            model.topModel.socket.BeginReceive(createReceived, 0);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void JoinButton_Click(object sender, EventArgs e)
         {
-            model.topModel.socket.BeginSend(FileNameTextBox.Text, (f, p) => { }, 0);
-            model.topModel.socket.BeginReceive(lineReceived, 0);
+            String message = "JOIN\n";
+            message += "Name:" + FileNameTextBox.Text + "\n";
+            message += "Password:" + PasswordTextBox.Text + "\n";
+            model.topModel.socket.BeginSend(message, (f, p) => { }, 0);
+            model.topModel.socket.BeginReceive(joinReceived, 0);
         }
 
-        private void lineReceived(String s, Exception e, object p)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        private void createReceived(String s, Exception e, object p)
         {
-
+            // CREATE SP OK/FAIL LF
+            // Name:name LF
+            // (Passowrd:password LF) / (message LF)
+            if (s.Contains("FAIL"))
+            {
+                DialogResult result = MessageBox.Show(s, "ERROR", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (result == DialogResult.Cancel)
+                {
+                    model.topModel.socket.CloseSocket();
+                    this.Close();
+                }
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show(s, "Confirm");
+                if (result == DialogResult.Cancel)
+                {
+                    model.topModel.socket.CloseSocket();
+                    this.Close();
+                }
+                else if (result == DialogResult.Yes)
+                {
+                    // go open the file
+                }
+            }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        private void joinReceived(String s, Exception e, object p)
+        {
+            // JOIN SP OK/FAIL LF
+            // Name:name LF
+            // (Version:version LF) / (message LF)
+            // (Length:length LF)
+            // (xml LF)
+            if (s.Contains("FAIL"))
+            {
+                DialogResult result = MessageBox.Show(s, "ERROR", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (result == DialogResult.Cancel)
+                {
+                    model.topModel.socket.CloseSocket();
+                    this.Close();
+                }
+            }
+
+            else
+            {
+                DialogResult result = MessageBox.Show(s, "Confirm", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Cancel)
+                {
+                    model.topModel.socket.CloseSocket();
+                    this.Close();
+                }
+                else if (result == DialogResult.Yes)
+                {
+                    // go open the file
+                }
+            }
+        } // End of "JoinReceived" method /////////////////////////////////////////////
     }
 }
