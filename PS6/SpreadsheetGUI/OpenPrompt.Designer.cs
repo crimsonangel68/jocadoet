48dc41be39e7ff15770f303fff0e4a19613d49ea
Calvin Kern <calvinmkern@gmail.com>
2013-04-21 22:01:02 -0600

    CLIENT SIDE

--- PS6/SpreadsheetGUI/OpenPrompt.Designer.cs
+++ PS6/SpreadsheetGUI/OpenPrompt.Designer.cs
@@ -54,7 +54,7 @@
             this.JoinButton.Location = new System.Drawing.Point(248, 254);
             this.JoinButton.Name = "JoinButton";
             this.JoinButton.Size = new System.Drawing.Size(75, 23);
-            this.JoinButton.TabIndex = 1;
+            this.JoinButton.TabIndex = 2;
             this.JoinButton.Text = "Join";
             this.JoinButton.UseVisualStyleBackColor = true;
             this.JoinButton.Click += new System.EventHandler(this.JoinButton_Click);
@@ -81,7 +81,6 @@
             // 
             // textBox2
             // 
-            this.textBox2.AcceptsTab = true;
             this.textBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(4, 3);
